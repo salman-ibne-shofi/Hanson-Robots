e5da81f96c1470fb3c487a39ec0098df41b856d9
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-12-01 19:24:55 +0600

    added social our partners and social media icon section

--- script.js
+++ script.js
@@ -15,11 +15,6 @@ const swiper = new Swiper(".swiper", {
 		clickable: true,
 	},
 
-	// And if we need scrollbar
-	scrollbar: {
-		el: ".swiper-scrollbar",
-	},
-
 	slidesPerView: "auto",
 	spaceBetween: 30,
 });
@@ -29,20 +24,29 @@ const swiperBrands = new Swiper(".swiper-brands", {
 	loop: true,
 
 	autoplay: {
-		delay: 1000, // Slide every 3 seconds
-		disableOnInteraction: true, // Pause autoplay after user interaction
+		delay: 1000, // Slide every 1 seconds
+		disableOnInteraction: false, // Pause autoplay after user interaction
 	},
 
 	speed: 2000,
+	slidesPerView: 6,
+	spaceBetween: 30,
+});
 
-	// And if we need scrollbar
-	scrollbar: {
-		el: ".swiper-scrollbar",
-	},
+// Swiper pertners wrapper
+const swiperPartners = new Swiper(".swiper-partners", {
+	// Optional parameters
+	loop: true,
+    freeMode: true,
+    freeModeMomentum: false,
 
 	autoplay: {
-		disableOnInteraction: false, // Continue autoplay after user interaction
+		delay: 0, // Slide 0 second
+		disableOnInteraction: false, // Pause autoplay after user interaction
 	},
+
+	speed: 1000,
+
 
 	slidesPerView: 6,
 	spaceBetween: 30,
